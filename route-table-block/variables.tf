8cb2cc320869dd5b25cd260f23a0b6ef869256e2
cbn1011 <90424228+cbn1011@users.noreply.github.com>
2023-07-13 21:11:35 +0530

    Update variables.tf

--- route-table-block/variables.tf
+++ route-table-block/variables.tf
@@ -1,6 +1,6 @@
 variable "project" {
   type = string
-  default = "MyProject"
+  default = "Bestdoc-UAE"
 }
 variable "vpcid" {
   type = string
@@ -18,8 +18,10 @@ variable "gateway-priv" {
   type = string
 }
 variable "subnet_association-priv" {
-  type = string
+  type = list(string)
+  default = ["190.160.1.0/24","190.160.2.0/24","190.160.3.0/24"]
 }
 variable "subnet_association-pub" {
-  type = string
+  type = list(string)
+  default = ["190.160.4.0/24","190.160.5.0/24","190.160.6.0/24"]
 }
