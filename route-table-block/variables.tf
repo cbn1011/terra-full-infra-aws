c822a802c9b7556337f77f6fab1f9694faac8d80
cbn1011 <90424228+cbn1011@users.noreply.github.com>
2023-07-13 21:29:59 +0530

    Update variables.tf

--- route-table-block/variables.tf
+++ route-table-block/variables.tf
@@ -17,11 +17,3 @@ variable "gateway" {
 variable "gateway-priv" {
   type = string
 }
-variable "subnet_association-priv" {
-  type = list(string)
-  default = ["190.160.1.0/24","190.160.2.0/24","190.160.3.0/24"]
-}
-variable "subnet_association-pub" {
-  type = list(string)
-  default = ["190.160.4.0/24","190.160.5.0/24","190.160.6.0/24"]
-}
